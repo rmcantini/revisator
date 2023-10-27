f19c33de6ea5417b21df6ba3479c20ae994ae3a9
Cantini <rmcantini@gmail.com>
2023-10-27 01:16:52 -0300

    easyocr gpu sucesso total

---
+++ revisator_easyocr_01.py
@@ -0,0 +1,12 @@
+"""Super python webapp for image checking
+"""
+import easyocr
+
+reader = easyocr.Reader(['en', 'pt'], gpu=True)
+results = reader.readtext('images\\01.png')
+
+TEXT = ' '
+for result in results:
+    TEXT += result[1] + ' '
+
+print(TEXT)
