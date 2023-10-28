48faa9f710150b2a94174a0db444048910b13fec
Cantini <rmcantini@gmail.com>
2023-10-28 00:47:18 -0300

    tesseract kicking as

--- revisator_tesseract_01.py
+++ revisator_tesseract_01.py
@@ -1,1 +1,11 @@
-C:\Program Files\Tesseract-OCR
+import pytesseract
+import cv2
+
+PATH = r"C:\Program Files\Tesseract-OCR"
+pytesseract.pytesseract.tesseract_cmd = PATH + r"\tesseract.exe"
+
+image = cv2.imread(r"images\01.png")
+
+text = pytesseract.image_to_string(image, lang="por")
+
+print(text)
