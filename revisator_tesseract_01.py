22802e6d7008a9b1468641a890c9192b874f316c
Cantini <rmcantini@gmail.com>
2023-10-28 00:49:30 -0300

    tesserac coments and format

--- revisator_tesseract_01.py
+++ revisator_tesseract_01.py
@@ -1,11 +1,11 @@
 import pytesseract
 import cv2
 
+# windows install
 PATH = r"C:\Program Files\Tesseract-OCR"
 pytesseract.pytesseract.tesseract_cmd = PATH + r"\tesseract.exe"
 
+# actual code
 image = cv2.imread(r"images\01.png")
-
 text = pytesseract.image_to_string(image, lang="por")
-
 print(text)
